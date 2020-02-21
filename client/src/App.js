4fe2dc375b9613d43c9c927f2aa39537e5601295
Syed Ahmad Tirmazi <syed.aham.tirmazi@ibm.com>
2020-02-21 23:22:42 +0400

    Update and delete todo items

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,20 @@ import React from "react";
 import "./App.css";
 
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import Paper from "@material-ui/core/Paper";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const LIST_TODO_ITEMS = gql`
   {
@@ -16,8 +27,22 @@ const LIST_TODO_ITEMS = gql`
   }
 `;
 
+const UPDATE_TODO_ITEM = gql`
+  mutation($id: ID!, $isComplete: Boolean!) {
+    update(id: $id, isComplete: $isComplete)
+  }
+`;
+
+const REMOVE_TODO_ITEM = gql`
+  mutation($id: ID!) {
+    remove(id: $id)
+  }
+`;
+
 function App() {
   const { loading, error, data } = useQuery(LIST_TODO_ITEMS);
+  const [update] = useMutation(UPDATE_TODO_ITEM);
+  const [remove] = useMutation(REMOVE_TODO_ITEM);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -25,13 +50,44 @@ function App() {
   return (
     <div style={{ display: "flex" }}>
       <div style={{ margin: "auto", width: 400 }}>
-        <Paper elevation={1}>
-          {data.list.map(({ id, text, isComplete }) => (
-            <div key={`${id}-todo-item`}>
-              {text} ({isComplete ? "Done" : "Pending"})
-            </div>
-          ))}
-        </Paper>
+        <List>
+          {data.list.map(({ id, text, isComplete }) => {
+            const labelId = `checkbox-list-label-${id}`;
+            return (
+              <ListItem
+                key={id}
+                role={undefined}
+                dense
+                button
+                onClick={() => {
+                  update({ variables: { id, isComplete: !isComplete } });
+                }}
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={isComplete}
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={text} />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => {
+                      remove({ variables: { id } });
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
       </div>
     </div>
   );
